0735e6a63167add672c2fc8f1c23f2febd4fb6af
macrosann <31016530+MACROSANN@users.noreply.github.com>
2023-02-16 20:48:39 -0300

    Atualizando via VSCode

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -7,42 +7,39 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		float a, b;
+		int a, b;
 		
 		System.out.println("Digite o primeiro valor: ");
 		a = scan.nextInt();
 		System.out.println("Digite o segundo valor: ");
 		b = scan.nextInt();
 		
-		float soma = soma(a, b);
-		float subtracao = subtracao(a, b);
-		float multiplicacao = multiplicacao(a, b);
+		int soma = soma(a, b);
+		int subtracao = subtracao(a, b);
+		int multiplicacao = multiplicacao(a, b);
 		float divisao = divisao(a, b);
 		
 		System.out.println("soma " + soma);
 		System.out.println("subtracao " + subtracao);
 		System.out.println("multiplicacao " + multiplicacao);
 		System.out.println("divisao " + divisao);
-		
 
 	}
 	
-	public static float soma(float a, float b) {
-		return a + b;
+	public static int soma(int x, int y) {
+		return x + y;
 	}
 
-	public static float subtracao(float a, float b) {
-		return a - b;
+	public static int subtracao(int x, int y) {
+		return x - y;
 	}
 
-	public static float multiplicacao(float a, float b) {
-		return a * b;
+	public static int multiplicacao(int x, int y) {
+		return x * y;
 	}
 
-	public static float divisao(float a, float b) {
-		return a / b;
+	public static float divisao(float x, float y) {
+		return x / y;
 	}
 	
-	
-
 }
